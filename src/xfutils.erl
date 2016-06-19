ac45cf7d148ca000f05065951f77bd0361f34ed2
simonxu <simon.xu72@gmail.com>
2016-06-19 22:02:26 +0800

    add datetime module exports used in payment_gateway

--- src/xfutils.erl
+++ src/xfutils.erl
@@ -1,11 +1,37 @@
 -module(xfutils).
 
 %% API exports
--export([]).
+%% datetime module exports
+-export([now/0,now/1,
+		 yesterday/0,yesterday/1
+		]).
+
+%% txn module exports
+-export([get_new_order_id/0]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
+now() ->
+		datetime:now().
+
+now(Type) -> 
+		datetime:now(Type).
+
+yesterday() ->
+		datetime:yesterday().
+
+yesterday(Type) ->
+		datetime:yesterday(Type).
+
+today() ->
+		datetime:today().
+
+today(Type) ->
+		datetime:today(Type).
+
+get_new_order_id() ->
+		datetime:get_new_order_id().
 
 
 %%====================================================================
