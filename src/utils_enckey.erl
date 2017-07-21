52f027763af171a635cdfabebb68cee3c8e3172f
simonxu72 <simon.xu72@gmail.com>
2017-07-21 19:04:25 +0800

    change load_private_key/load_public_key logic according to orig unionpay_config.erl

--- src/utils_enckey.erl
+++ src/utils_enckey.erl
@@ -7,6 +7,7 @@
 %%% Created : 20. 七月 2017 21:40
 %%%-------------------------------------------------------------------
 -module(utils_enckey).
+-include_lib("public_key/include/public_key.hrl").
 -author("simon").
 
 %% API
@@ -24,14 +25,22 @@ load_private_key(KeyFileName, Pwd) ->
   {ok, PemBin} = file:read_file(KeyFileName),
   [RSAEntry | _Rest] = public_key:pem_decode(PemBin),
   RsaKeyInfo = public_key:pem_entry_decode(RSAEntry, Pwd),
+  RsaKey = public_key:der_decode('RSAPrivateKey', RsaKeyInfo#'PrivateKeyInfo'.privateKey),
   lager:debug("private key = ~p", [RsaKeyInfo]),
-  {ok, RsaKeyInfo}.
+  RsaKey.
+%%  lager:debug("private key = ~p", [RsaKeyInfo]),
+%%  {ok, RsaKeyInfo}.
 
 
 %%==================================================================
 load_public_key(KeyFileName) ->
   {ok, PemBin} = file:read_file(KeyFileName),
   [Certificate] = public_key:pem_decode(PemBin),
-  PublicKey = public_key:pem_entry_decode(Certificate),
+  {_, DerCert, _} = Certificate,
+  Decoded = public_key:pkix_decode_cert(DerCert, otp),
+  PublicKey = Decoded#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subjectPublicKeyInfo#'OTPSubjectPublicKeyInfo'.subjectPublicKey,
   lager:debug("public key = ~p", [PublicKey]),
-  {ok, PublicKey}.
+  PublicKey.
+%%  PublicKey = public_key:pem_entry_decode(Certificate),
+%%  lager:debug("public key = ~p", [PublicKey]),
+%%  {ok, PublicKey}.
